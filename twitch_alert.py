708aa7df498469444b93e33f1aba46db2668ab12
LemonzPCMR <LemonzPCMR@protonmail.com>
2023-08-22 11:53:52 +1200

    Twitch ping control

--- twitch_alert.py
+++ twitch_alert.py
@@ -6,13 +6,13 @@ from database import retrieve_all_data_from_guild_table
 # This dictionary will keep track of streams' timestamps.
 stream_timestamps = {}
 
-
-@tasks.loop(minutes=1)  # Check every 10 minutes
+@tasks.loop(minutes=1)  # Check every 1 minutes
 async def check_twitch_streams(bot, config):
     print("Checking Twitch streams.")
 
     ALERT_CHANNEL_ID = int(config['TWITCH']['ALERT_CHANNEL_ID'])
     ROLE_TO_PING = int(config['TWITCH']['ROLE_TO_PING'])
+    ENABLE_TWITCH_PING = config['DEFAULT'].getboolean('enable_twitch_ping', fallback=False)
 
     channel = bot.get_channel(ALERT_CHANNEL_ID)
     role = bot.get_guild(channel.guild.id).get_role(ROLE_TO_PING)
@@ -28,12 +28,12 @@ async def check_twitch_streams(bot, config):
         # Check if the timestamp has changed or if it's a new streamer we haven't seen.
         if is_live and (username not in stream_timestamps or stream_timestamps[username] != current_timestamp):
             embed = create_live_embed(stream_data, custom_message)
-            await channel.send(f"{role.mention}", embed=embed)
+            message_content = f"{role.mention}" if ENABLE_TWITCH_PING else ""
+            await channel.send(message_content, embed=embed)
             stream_timestamps[username] = current_timestamp
 
         elif not is_live and username in stream_timestamps:
             del stream_timestamps[username]  # Remove the streamer from the timestamp dict if they're offline.
-
 
 async def start_twitch_alerts(bot, config):
     check_twitch_streams.start(bot, config)
