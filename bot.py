9afaa71890f518f5801e56ed9aff86d4daecac5d
LemonzPCMR <LemonzPCMR@protonmail.com>
2023-09-01 12:48:10 +1200

    Multi-server support

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import discord
 from discord import app_commands
 from discord.ext import tasks
 import configparser
-from twitch_alert import start_twitch_alerts
 from twitch_auth import refresh_twitch_token
 from twitch_api import update_headers_with_new_token
 from database import initialize_guild_tables, load_guild_data_into_memory
 from commands import setup
+from twitch_alert import start_twitch_alerts
+from utils import force_load_guild as flg
 
 # Load configuration
 config = configparser.ConfigParser()
